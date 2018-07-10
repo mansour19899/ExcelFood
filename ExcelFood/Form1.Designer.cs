a4c9da921f7e0e843da069d1e44396e8e1f10d82
mansour <smm.2020@gmail.com>
2018-07-10 15:32:29 +0430

    start

--- ExcelFood/Form1.Designer.cs
+++ ExcelFood/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(43, 37);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(155, 42);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(35, 13);
+            this.lblFilePath.TabIndex = 1;
+            this.lblFilePath.Text = "label1";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(43, 130);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 315);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Button btnRead;
     }
 }
 
--- ExcelFood/Form1.cs
+++ ExcelFood/Form1.cs
@@ -7,14 +7,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelFood
 {
     public partial class Form1 : Form
     {
+        string filePath = "";
+        PoonehEntities db = null;
+        List<TraySchedule> list = new List<TraySchedule>();
         public Form1()
         {
             InitializeComponent();
+            db = new PoonehEntities();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "File 1";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //save selected file name
+                filePath = openFileDialog1.FileName;
+                lblFilePath.Text = filePath;
+                int x = 0;
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            //open excel
+            Excel.Application xlApp = new Excel.Application();
+            //open workbook
+            Excel.Workbook workbook = xlApp.Workbooks.Open(filePath);
+            //open sheet
+            Excel._Worksheet sheet = workbook.Sheets[2];
+            Excel.Range excelFile = sheet.UsedRange;
+            // int row = int.Parse(txtRow.Text), column = int.Parse(txtColumn.Text);
+            int row = 20, column = 5;
+
+            
+
+            bool x = true;
+            int i = 1;
+            while (x)
+            {
+                if (excelFile.Cells[i, 1].Value2 != null)
+                {
+
+                }
+                else
+                    x = false;
+            }
+
+
+
+
+
+
+
+
+            var t = excelFile.Cells[row, column];
+            var tt = excelFile.Cells[row, column].Value2;
+            var ttt = excelFile.Cells[row, column].Value;
+
+
+
+
+
+
+            MessageBox.Show("Value:  " + excelFile.Cells[row, column].Value2.ToString());
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            ////release com objects to fully kill excel process from running in the background
+            //Marshal.ReleaseComObject(excelFile);
+            ////close and release
+            ////workbook.Close();
+            //Marshal.ReleaseComObject(workbook);
+            ////quit and release
+            //xlApp.Quit();
+            //Marshal.ReleaseComObject(xlApp);
+
+
         }
     }
 }
